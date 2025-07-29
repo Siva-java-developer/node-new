fbc96c4a84abbad4b9ad6c58f1fc774538eb70b9
Siva P <psiva4064@gmail.com>
2025-07-29 21:18:57 +0530

    Rfactor : mail

--- utils/sendOtpMail.js
+++ utils/sendOtpMail.js
@@ -16,11 +16,11 @@ const sendOtpEmail = async (to, otp) => {
       <div style="max-width: 600px; margin: 0 auto; background: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);">
         
         <!-- Header -->
-            <div style="background-color: #868a88ff; padding: 20px 30px;">
+            <div style="background-color: #ffffff; padding: 20px 30px;">
             <table width="100%" cellpadding="0" cellspacing="0" style="margin-bottom: 0;">
               <tr>
                 <td style="text-align: left;">
-                  <h1 style="color: #ffffff; margin: 0; font-size: 22px;">Skill Up Tech Solutions</h1>
+                  <h1 style="color: #f57f17; margin: 0; font-size: 22px;">Skill Up Tech Solutions</h1>
                 </td>
                 <td style="text-align: right;">
                   <img src="https://frontend-admin-panel-ecru.vercel.app/assets/newlogo-CpmjAHNb.png" alt="Skill Up Tech Logo" style="width: 50px; object-fit: cover;">
@@ -30,11 +30,11 @@ const sendOtpEmail = async (to, otp) => {
           </div>
 
         <!-- Body -->
-        <div style="padding: 30px; color: #333333;">
-          <p style="font-size: 16px; color: #6b21a8;">Dear User,</p>
+        <div style="padding: 30px; color: #000000ff;">
+          <p style="font-size: 16px; color: #000000ff;">Dear User,</p>
           <p style="font-size: 16px;">Your One Time Password (OTP) is:</p>
           
-          <div style="font-size: 28px; font-weight: bold; color: #909692ff; text-align: left; margin: 20px 0;">
+          <div style="font-size: 28px; font-weight: bold; color: #f57f17; text-align: left; margin: 20px 0;">
             ${otp}
           </div>
 
@@ -47,7 +47,7 @@ const sendOtpEmail = async (to, otp) => {
         <!-- Footer -->
         <div style="background-color: #f1f1f1; padding: 20px 30px; text-align: center; font-size: 13px; color: #555; margin-top: 40px;">
           <hr style="border: none; border-top: 1px solid #ccc; margin-bottom: 15px;">
-          <p style="margin: 0 0 5px;">&copy; 2025 <a href="https://skilluptechbuzz.in/" style="color: #898e8bff; text-decoration: none;">Skill Up Tech Solutions</a>. All rights reserved.</p>
+          <p style="margin: 0 0 5px;">&copy; 2025 <a href="https://skilluptechbuzz.in/" style="color: #f57f17; text-decoration: none;">Skill Up Tech Solutions</a>. All rights reserved.</p>
           <p style="margin: 0 0 5px;">This is an auto-generated email. Do not reply.</p>
         </div>
 
